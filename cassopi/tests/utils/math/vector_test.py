36880d2e06aa69204606400b350f9793be60cbf6
bebraw <bebraw@1e1c859f-4354-0410-b355-3795c12af104>
2008-08-29 08:07:35 +0000

    Added test list for vector class.

---
+++ cassopi/tests/utils/math/vector_test.py
@@ -0,0 +1,11 @@
+from unittest import TestCase
+
+from cassopi.utils.math.vector import Vector
+
+class TestVector(TestCase):
+    def test_init(self):
+        vector = Vector()
+        print 'foobar'
+        assert False
+    
+    # add, subtract, mul, div, dot, length, normalize
